721a8179c3d1ee23da2ff716281f70124559ea89
Tarun Prakash <tarunprakash2468@gmail.com>
2025-07-04 23:12:22 -0700

    perf: removed duplicate asset functions

--- core/import_udl_to_nominal.py
+++ core/import_udl_to_nominal.py
@@ -129,29 +129,6 @@ asset = client.create_asset(
 
 # --- UPLOAD DATA ---
 
-# Retrieve asset
-def retrieve_asset(
-    client: nominal.NominalClient,
-    platform: str,
-    serial_num: str
-) -> nominal.Asset:
-    properties = {"platform": platform, "serial_num": serial_num}
-    existing_assets = client.search_assets(
-        properties=properties
-    )
-    if len(existing_assets) > 1:
-        raise RuntimeError(
-            f"Too many assets ({len(existing_assets)}) found with properties {properties}"
-        )
-    elif len(existing_assets) == 0:
-        raise RuntimeError(
-            f"No such asset found with properties {properties}"
-        )
-    else:
-        return existing_assets[0]
-
-asset = retrieve_asset(client, platform="satellite", serial_num=satellite_number)
-
 # Create dataset
 dataset = client.create_dataset(
     name="State Vectors",
@@ -178,7 +155,7 @@ asset.add_dataset(
 
 # Create run
 run = client.create_run(
-    name=f"Historial Data between {start_time} and {end_time}",
+    name=f"Historical Data between {start_time} and {end_time}",
     start=start_dt,
     end=end_dt,
     properties={
